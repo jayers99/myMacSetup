72f6aad89345ee4b4291dd0a890a32fdd7bf65fd
John Ayers <jayers99@gmail.com>
2018-12-24 11:07:42 -0800

    add all the  stuff to install

---
+++ brew.sh
@@ -0,0 +1,36 @@
+# https://docs.brew.sh/
+
+# snippets for brew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
+
+# First update the formulae and Homebrew itself:
+brew --version
+brew update
+
+# installed apps
+brew list --help
+brew list --versions
+brew cask list
+# That is packages that are not dependencies
+brew leaves
+
+# You can now find out what is outdated with:
+brew outdated
+
+# Upgrade everything with:
+brew upgrade
+
+# By default, Homebrew does not uninstall old versions of a formula, so over time you will accumulate old versions. To remove them, simply use:
+brew cleanup <formula>
+# or to see what would be cleaned up:
+brew cleanup -n
+# clean up all
+brew cleanup
+
+# check for problems
+brew doctor
+
+# should create these dirs
+ll /usr/local/sbin
+sudo mkdir -p /usr/local/sbin
+sudo chown -R $(whoami) /usr/local/sbin
