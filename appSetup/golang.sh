7d4ff7ba445061eb03775c5cd153d557b442d889
John Ayers <jayers99@gmail.com>
2019-01-19 14:30:48 -0800

    add golang

---
+++ appSetup/golang.sh
@@ -0,0 +1,14 @@
+brew install go
+
+==> Downloading https://homebrew.bintray.com/bottles/go-1.11.4.mojave.bottle.tar.gz
+######################################################################## 100.0%
+==> Pouring go-1.11.4.mojave.bottle.tar.gz
+==> Caveats
+A valid GOPATH is required to use the `go get` command.
+If $GOPATH is not specified, $HOME/go will be used by default:
+  https://golang.org/doc/code.html#GOPATH
+
+You may wish to add the GOROOT-based install location to your PATH:
+  export PATH=$PATH:/usr/local/opt/go/libexec/bin
+==> Summary
+🍺  /usr/local/Cellar/go/1.11.4: 9,298 files, 404.3MB
---
+++ appSetup/grep.sh
@@ -0,0 +1,32 @@
+> $ brew install grep
+==> Installing dependencies for grep: pcre
+==> Installing grep dependency: pcre
+==> Downloading https://homebrew.bintray.com/bottles/pcre-8.42.mojave.bottle.tar.gz
+######################################################################## 100.0%
+==> Pouring pcre-8.42.mojave.bottle.tar.gz
+🍺  /usr/local/Cellar/pcre/8.42: 204 files, 5.5MB
+==> Installing grep
+==> Downloading https://homebrew.bintray.com/bottles/grep-3.3.mojave.bottle.1.tar.gz
+######################################################################## 100.0%
+==> Pouring grep-3.3.mojave.bottle.1.tar.gz
+==> Caveats
+All commands have been installed with the prefix "g".
+If you need to use these commands with their normal names, you
+can add a "gnubin" directory to your PATH from your bashrc like:
+  PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+
+Additionally, you can access their man pages with normal names if you add
+the "gnuman" directory to your MANPATH from your bashrc as well:
+  MANPATH="/usr/local/opt/grep/libexec/gnuman:$MANPATH"
+==> Summary
+🍺  /usr/local/Cellar/grep/3.3: 21 files, 884.8KB
+==> Caveats
+==> grep
+All commands have been installed with the prefix "g".
+If you need to use these commands with their normal names, you
+can add a "gnubin" directory to your PATH from your bashrc like:
+  PATH="/usr/local/opt/grep/libexec/gnubin:$PATH"
+
+Additionally, you can access their man pages with normal names if you add
+the "gnuman" directory to your MANPATH from your bashrc as well:
+  MANPATH="/usr/local/opt/grep/libexec/gnuman:$MANPATH"
