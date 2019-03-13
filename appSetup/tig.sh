4e9997711327285989a794f0bae257e7a7fb29cd
John Ayers <jayers99@gmail.com>
2019-03-13 11:32:31 -0700

    add tig

---
+++ appSetup/tig.sh
@@ -0,0 +1,16 @@
+brew install tig
+==> Downloading https://homebrew.bintray.com/bottles/tig-2.4.1_1.mojave.bottle.tar.gz
+==> Pouring tig-2.4.1_1.mojave.bottle.tar.gz
+==> Caveats
+A sample of the default configuration has been installed to:
+  /usr/local/opt/tig/share/tig/examples/tigrc
+to override the system-wide default configuration, copy the sample to:
+  /usr/local/etc/tigrc
+
+Bash completion has been installed to:
+  /usr/local/etc/bash_completion.d
+
+zsh completions and functions have been installed to:
+  /usr/local/share/zsh/site-functions
+==> Summary
+🍺  /usr/local/Cellar/tig/2.4.1_1: 15 files, 855.7KB
